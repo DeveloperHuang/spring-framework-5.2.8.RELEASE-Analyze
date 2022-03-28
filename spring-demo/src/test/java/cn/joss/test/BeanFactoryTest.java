42e020d1c8815985b6a3e2a806599455241e30aa
huangjianqiu <511867317@qq.com>
2022-03-28 14:41:32 +0800

    1、测试代码迁移至test，并修改xml获取逻辑，改为通过ClassPathXmlApplicationContext获取

--- spring-demo/src/test/java/cn/joss/test/BeanFactoryTest.java
+++ spring-demo/src/test/java/cn/joss/test/BeanFactoryTest.java
@@ -1,29 +1,28 @@
+package cn.joss.test;
+
 import cn.joss.test.annotation.DemoService;
 import cn.joss.test.annotation.TestConfig;
 import cn.joss.test.xml.TestBean;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by sadwx on 17/4/19.
  */
 public class BeanFactoryTest {
 
-	public static void main(String[] args) {
-//		annotationConfig();
-		xmlFactory();
-//
-    }
+	private static Class<?> CLAZZ = BeanFactoryTest.class;
 
-    public static void annotationConfig(){
+	@Test
+    public void annotationConfig(){
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(TestConfig.class);
 		DemoService demoService = applicationContext.getBean(DemoService.class);
 		System.out.println(demoService);
 	}
-
-	public static void xmlFactory(){
-		XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
+	@Test
+	public void xmlFactory(){
+		ClassPathXmlApplicationContext beanFactory = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         TestBean testBean = beanFactory.getBean(TestBean.class);
         System.out.println(testBean.getName());
